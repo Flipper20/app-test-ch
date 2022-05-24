47bb46e25d0f15b59e2551ba5ce57563388e3f5f
Flipper20 <98557779+Flipper20@users.noreply.github.com>
2022-05-24 18:17:55 -0300

    Actualización para otra clase

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+function NavBar() {
+    
+    return(
+        <div class="topnav">
+        <ul class="horizontal customText" aria-labelledby="navbarDropdownMenuLink">
+                <li><a href="./op1.html">Opción-1</a></li>
+                <li><a href="./op2.html">Opción-2</a></li>
+                <li><a href="./op3.html">Opción-3</a></li>
+        </ul>
+    </div>
+        
+        /*Esta lista fue la que hice en desarrollo web*/
+    )
+
+}
+
+export default NavBar;
